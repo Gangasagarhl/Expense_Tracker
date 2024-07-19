6fbeb7b44a6e94ef1176448ac9643c15cda50311
Gangasagarhl <gangasagarhl1112@gmail.com>
2024-07-19 16:30:37 +0530

    sagars

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from .views import index
 
 urlpatterns = [
-    path("",include('tracker.urls')),
+    path("",index,name ="index" ),
     path('admin/', admin.site.urls),
 ]
